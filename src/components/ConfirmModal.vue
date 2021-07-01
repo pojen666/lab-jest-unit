51cebc7154752f58e11a4c1b9162d6726442b69d
pojen <pzyang2004@gmail.com>
2021-07-01 13:51:52 +0800

    在dialog補上area-hidden和role阻止擋到原本的頁面

--- src/components/ConfirmModal.vue
+++ src/components/ConfirmModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="modal modal-dialog-centered fade" ref="modal" tabindex="-1">
+  <div class="modal fade" ref="modal" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
