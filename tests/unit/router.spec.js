3e351757310c4284cf87664d866529d4163c2615
pojen <pzyang2004@gmail.com>
2021-07-01 17:16:06 +0800

    添加針對router的測試

---
+++ tests/unit/router.spec.js
@@ -0,0 +1,35 @@
+import {mount, createLocalVue} from '@vue/test-utils'
+import VueRouter from 'vue-router'
+import router from '@/router/index';
+import Vuex from 'vuex'
+import App from "@/App";
+import ModalView from "@/views/ModalView";
+import currentUnit from "@/store/modules/current-unit";
+import Home from "@/views/Home";
+
+const localVue = createLocalVue()
+localVue.use(VueRouter)
+localVue.use(Vuex)
+
+
+describe('router', () => {
+
+    it('test routing by router', async () => {
+        const store = new Vuex.Store({
+            modules: {
+                currentUnit
+            }
+        })
+        const wrapper = mount(App, { localVue, router, store });
+        expect(wrapper.findComponent(ModalView).exists()).toBe(false);
+        expect(wrapper.findComponent(Home).exists()).toBe(true);
+
+        await router.push('/modal');
+        expect(wrapper.findComponent(ModalView).exists()).toBe(true);
+        expect(wrapper.findComponent(Home).exists()).toBe(false);
+
+        await router.push('/');
+        expect(wrapper.findComponent(ModalView).exists()).toBe(false);
+        expect(wrapper.findComponent(Home).exists()).toBe(true);
+    })
+})
