9e98413ec81d133bdc896176390c7f4d66bfd7f7
pojen <pzyang2004@gmail.com>
2021-07-25 00:53:53 +0800

    調整vue.config設定符合github線上demo的格式

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    publicPath: './',
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/lab-vue-unit/'
+        : '/',
     configureWebpack:{
         entry:'./src/main.js',
         optimization: {
